25a3426fbe0d7ccce33b2d85fa798733294acbb5
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-01-16 14:53:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 # Bot token from Bot Father
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1444782765:AAGC1IIKYi0UBedn8HU3O9BUf8aQziOQF3o"
 
 # Your API ID and Hash from https://my.telegram.org/apps
-API_ID = 123123
-API_HASH = ""
+API_ID = 2391388
+API_HASH = "75c3fb34752b710b6106b8f510d06ec4"
 
 # Chat used for logs
-log_chat = 123456789
+log_chat = -1001237251568
 
 
 # Sudoers and super sudoers
-super_sudoers = [123456789]
-sudoers = [987654321]
+super_sudoers = [1352720951]
+sudoers = [1352720951]
 sudoers += super_sudoers
 
 
